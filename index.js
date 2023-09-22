adbfcbf602f61cc6d594e00d34a2b1e79be43cd1
unknown <hasnainalam1166@gmail.com>
2023-09-22 05:52:00 +0500

    ready to deploy

--- index.js
+++ index.js
@@ -1,41 +1,41 @@
 const express = require("express");
-const dotEnv = require("dotenv");
-const cors = require("cors");
+// const dotEnv = require("dotenv");
+// const cors = require("cors");
 const bodyParser = require("body-parser");
-const compression = require('compression');
-const connectToDatabase = require("./database/DBconnection");
+// const compression = require('compression');
+// const connectToDatabase = require("./database/DBconnection");
 
-const cacheMiddleware = require("./middlewares/cachingMiddleware");
+// const cacheMiddleware = require("./middlewares/cachingMiddleware");
 const app = express();
 
 
-const AuthRoute = require('./routes/auth');
-const OrderRoute = require('./routes/order');
-const ReviewRoute = require('./routes/review');
+// const AuthRoute = require('./routes/auth');
+// const OrderRoute = require('./routes/order');
+// const ReviewRoute = require('./routes/review');
 
-const TestRouter = require('./routes/test');
-const PublicRouter = require('./controllers/public/getUsers');
+// const TestRouter = require('./routes/test');
+// const PublicRouter = require('./controllers/public/getUsers');
 
-app.use(cors());
+// app.use(cors());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
-app.use(compression());
-dotEnv.config();
+// app.use(compression());
+// dotEnv.config();
 
-cacheMiddleware();
+// cacheMiddleware();
 
-connectToDatabase();
+// connectToDatabase();
 
-app.use('/auth', AuthRoute);
-app.use('/order', OrderRoute);
-app.use('/review', ReviewRoute)
+// app.use('/auth', AuthRoute);
+// app.use('/order', OrderRoute);
+// app.use('/review', ReviewRoute)
 
 app.get('/', (req, res) => {
     return res.status(200).json({ msg: "hellow" })
 })
 
-app.use('/data', PublicRouter);
-app.use('/test', TestRouter);
+// app.use('/data', PublicRouter);
+// app.use('/test', TestRouter);
 
 app.listen(() => {
     console.log('server is live')
