1c382deb33a248b76665ec573692b99985058c92
unknown <hasnainalam1166@gmail.com>
2023-10-12 04:04:05 +0500

    changes

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ if (process.env.NODE_ENV !== "production") {
 
 const server = http.createServer(app);
 
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3000;
 
 server.listen(PORT, () => {
     logger.info(`Server running on port ${PORT}`);
